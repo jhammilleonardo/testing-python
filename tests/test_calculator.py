09f2066a5b9bf3993a0931d777ea32bbbb540529
jhammilleonardo <jhammil.leonardo@uab.edu.bo>
2025-06-05 15:09:50 -0400

    test de la multiplicacion

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.calculator import suma, resta
+from src.calculator import suma, resta, multiplicacion
 
 class CalculatorTest(unittest.TestCase):
 
@@ -10,4 +10,7 @@ class CalculatorTest(unittest.TestCase):
 
     def test_resta(self):
         self.assertEqual(resta(10, 5), 5)
-
+    
+    def test_multiplicacion(self):
+        self.assertEqual(multiplicacion(2, 3), 6)
+            
